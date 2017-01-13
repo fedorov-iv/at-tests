a602e8176b1a2081a0047cc66d8d1ea852c0121f
Igor Fedorov <ifedor@ymail.com>
2017-01-13 12:52:38 +0300

    some comments

--- src/test/java/ru/pflb/at/pages/LoginPage.java
+++ src/test/java/ru/pflb/at/pages/LoginPage.java
@@ -10,22 +10,43 @@ import static com.codeborne.selenide.Selenide.open;
  */
 public class LoginPage {
 
+    /**
+    * Возвращает элемент поля ввода логина
+    *@return  SelenideElement поле ввода логина
+    */
     public SelenideElement getUsernameField() {
         return $("#login-username");
     }
 
+    /**
+     * Возвращает элемент поля ввода пароля
+     *@return  SelenideElement поле ввода пароля
+     */
     public SelenideElement getPasswordField() {
         return $("#login-password");
     }
 
+    /**
+     * Возвращает элемент кнопки отправки формы
+     *@return  SelenideElement кнопка входа
+     */
     public SelenideElement getSubmitButton() {
         return $("#login-button");
     }
 
+    /**
+     * Переход на страницу авторизации
+     */
     public void navigate(){
         open(Config.baseUrl + "/matrixboard2");
     }
 
+
+    /**
+     * Заполенение полей логин и пароль и отправка формы авторизации
+     *@param username имя пользователя
+     *@param password пароль
+     */
     public void login(String username, String password){
 
         this.getUsernameField().setValue(username);
