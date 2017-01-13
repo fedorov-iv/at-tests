c32448853eaf7b331e8eb0e267c103419ecffa35
Igor Fedorov <ifedor@ymail.com>
2017-01-13 15:49:03 +0300

    top menu elements

--- src/test/java/ru/pflb/at/pages/InnerPage.java
+++ src/test/java/ru/pflb/at/pages/InnerPage.java
@@ -6,6 +6,8 @@ import ru.pflb.at.Config;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+import org.openqa.selenium.By;
+
 /**
  * Базовый класс внутренних страниц
  * Предоставляет доступ к основному меню и кнопке выхода
@@ -19,6 +21,51 @@ public class InnerPage {
      */
     public SelenideElement getLogoutButton() {
         return $("#logout span");
+    }
+
+    /**
+     * Возвращает элемент ссылки "Люди"
+     *
+     * @return SelenideElement ссылка "Люди"
+     */
+    public SelenideElement getEmployeeLink() {
+        return $(By.linkText("Люди"));
+    }
+
+    /**
+     * Возвращает элемент ссылки "Поиск"
+     *
+     * @return SelenideElement ссылка "Поиск"
+     */
+    public SelenideElement getSearchLink() {
+        return $(By.linkText("Поиск"));
+    }
+
+    /**
+     * Возвращает элемент ссылки "Должности"
+     *
+     * @return SelenideElement ссылка "Должности"
+     */
+    public SelenideElement getPositionsLink() {
+        return $(By.linkText("Должности"));
+    }
+
+    /**
+     * Возвращает элемент ссылки "Компетенции"
+     *
+     * @return SelenideElement ссылка "Компетенции"
+     */
+    public SelenideElement getQualificationsLink() {
+        return $(By.linkText("Компетенции"));
+    }
+
+    /**
+     * Возвращает элемент ссылки "Медали"
+     *
+     * @return SelenideElement ссылка "Медали"
+     */
+    public SelenideElement getMedalsLink() {
+        return $(By.linkText("Медали"));
     }
 
     public void navigate() {
